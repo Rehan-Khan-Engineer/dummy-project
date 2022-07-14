9a0761f00f250679a08d12982d0270d8d2243de0
Rehan-Khan-Engineer <rk116159@gmail.com>
2022-07-15 00:29:46 +0530

    updated-text

--- dummy-project/src/App.js
+++ dummy-project/src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Dummy project! will be deleted later.</div>;
+  return (
+    <div className="App">
+      Hello world! A Dummy project! will be deleted later.
+    </div>
+  );
 }
 
 export default App;
